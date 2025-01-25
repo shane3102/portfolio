8385b8168a9c05caa27c2a9b843abaa52ec97bf4
shane3102 <bkucharczyk2000@gmail.com>
2025-01-25 12:26:32 +0100

    Add basic background color change per planned pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,4 +16,17 @@ export class AppComponent {
   changeShownContent(shownContent: 'illchess' | 'leon' | 'inbox-outbox' | 'CV') {
     this.globalState.changeShownContent(shownContent)
   }
+
+  getBackgroundColorByShownContent(): string {
+    switch(this.globalState.currentShownContent()) {
+      case 'CV': 
+        return '#808080a1'
+      case 'illchess':
+        return '#964b00a1'
+      case 'leon':
+        return '#baff39a1'
+      case 'inbox-outbox':
+        return '#add8e6a1'     
+    }
+  }
 }
