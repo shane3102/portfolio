5f99603f039814ea23be41bdfbe7d546be1a1052
bartlomiej.kucharczy <bartlomiej.kucharczyk@envelo.pl>
2025-01-31 15:43:25 +0100

    add simple sidebar, change transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,15 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GlobalStore } from './state/GlobalStore';
 import { ProjectDescriptionLayoutComponent } from './component/project-description-layout/project-description-layout.component';
+import { NavigationSidebarComponent } from './component/navigation/navigation-sidebar/navigation-sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProjectDescriptionLayoutComponent],
+  imports: [
+    RouterOutlet, 
+    ProjectDescriptionLayoutComponent,
+    NavigationSidebarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
