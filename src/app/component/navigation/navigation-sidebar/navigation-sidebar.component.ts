b92fbf18339c6fe49ebd36f644409d4daee1b602
shane3102 <bucharczyk2000@gmail.com>
2025-02-02 22:29:20 +0100

    move color changing to sidebar instead of whole page

--- src/app/component/navigation/navigation-sidebar/navigation-sidebar.component.ts
+++ src/app/component/navigation/navigation-sidebar/navigation-sidebar.component.ts
@@ -19,4 +19,22 @@ export class NavigationSidebarComponent {
   changeShownContent(shownContent: 'illchess' | 'leon' | 'inbox-outbox' | 'CV') {
     this.globalState.changeShownContent(shownContent)
   }
+
+  getBackgroundColorByShownContent(): string {
+
+    if (this.globalState.changingContent()) {
+      return 'white'
+    }
+
+    switch (this.globalState.currentShownContent()) {
+      case 'CV':
+        return '#808080a1'
+      case 'illchess':
+        return '#964b00a1'
+      case 'leon':
+        return '#baff39a1'
+      case 'inbox-outbox':
+        return '#add8e6a1'
+    }
+  }
 }
