00a03f878c25074f97ac5c291086215d16af2c8d
shane3102 <bucharczyk2000@gmail.com>
2025-02-03 19:40:04 +0100

    add colors per project definitions with possible transparency adjustment

--- src/app/utils/ColorsPerProjectDefinition.ts
+++ src/app/utils/ColorsPerProjectDefinition.ts
@@ -1,8 +1,22 @@
-export let colorDefinitions: Map<string, string> =  new Map(
-    [
-        ['CV', '#808080a1'],
-        ['illchess', '#964b00a1'],
-        ['leon', '#baff39a1'],
-        ['inbox-outbox', '#add8e6a1']
-    ]
-)
+export class ColorsPerProjectDefinition {
+
+    static colorDefinitions: Map<string, number[]> =  new Map(
+        [
+            ['CV', [128, 128, 128]],
+            ['illchess', [150, 75, 0]],
+            ['leon', [186, 255, 57]],
+            ['inbox-outbox', [173, 216, 230]]
+        ]
+    )
+
+    public static getColorByProjectByTransparency(
+        contentIdentifier: 'illchess' | 'leon' | 'inbox-outbox' | 'CV',
+        transparency: number = 1
+    ) {
+        let foundColorDefinition = this.colorDefinitions.get(contentIdentifier)
+        let red = foundColorDefinition? foundColorDefinition[0] : 0
+        let green = foundColorDefinition? foundColorDefinition[1] : 0
+        let blue = foundColorDefinition? foundColorDefinition[2] : 0
+        return `rgba(${red}, ${green}, ${blue}, ${transparency})`
+    }
+}
