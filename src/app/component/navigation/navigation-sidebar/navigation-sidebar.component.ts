60495a7512e701594c75d45d6a7d4b5f9237dd57
shane3102 <bucharczyk2000@gmail.com>
2025-02-03 11:28:08 +0100

    remove useless style definition by signal, rename typo-ed component

--- src/app/component/navigation/navigation-sidebar/navigation-sidebar.component.ts
+++ src/app/component/navigation/navigation-sidebar/navigation-sidebar.component.ts
@@ -1,14 +1,14 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { GlobalStore } from '../../../state/GlobalStore';
-import { NavigationButtonComponent } from '../navigation-button/navigation-button.component';
 import { colorDefinitions } from '../../../utils/ColorsPerProjectDefinition';
-import { NaigationSeparatorComponent } from '../naigation-separator/naigation-separator.component';
+import { NavigationSeparatorComponent } from '../navigation-separator/navigation-separator.component';
+import { NavigationButtonComponent } from '../navigation-button/navigation-button.component';
 
 @Component({
   selector: 'app-navigation-sidebar',
   imports: [
     NavigationButtonComponent,
-    NaigationSeparatorComponent
+    NavigationSeparatorComponent
   ],
   templateUrl: './navigation-sidebar.component.html',
   styleUrl: './navigation-sidebar.component.scss'
@@ -16,8 +16,6 @@ import { NaigationSeparatorComponent } from '../naigation-separator/naigation-se
 export class NavigationSidebarComponent {
 
   globalState = inject(GlobalStore)
-
-  hover = signal(false)
 
   changeShownContent(shownContent: 'illchess' | 'leon' | 'inbox-outbox' | 'CV') {
     this.globalState.changeShownContent(shownContent)
@@ -30,7 +28,7 @@ export class NavigationSidebarComponent {
     }
     let foundColor = colorDefinitions.get(this.globalState.currentShownContent())
 
-    return  foundColor ? foundColor : 'white'
+    return foundColor ? foundColor : 'white'
 
   }
 }
