2ee00b319e3fc44fc7064732e8a5409c636d44fe
bartlomiej.kucharczy <bartlomiej.kucharczyk@envelo.pl>
2025-02-19 14:28:34 +0100

    add buttons for manual swiping of technologies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationSidebarComponent } from './component/navigation/navigation-sidebar/navigation-sidebar.component';
 import { ProjectDescriptionLayoutComponent } from './component/project-description-layout/project-description-layout.component';
+import { ProjectLayoutComponent } from './component/project/project-layout/project-layout.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet, 
     ProjectDescriptionLayoutComponent,
-    NavigationSidebarComponent
+    NavigationSidebarComponent,
+    ProjectLayoutComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
