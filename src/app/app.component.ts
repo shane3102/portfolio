9e4c1033ea0515e6d893033d65d095a957c34b64
shane3102 <bkucharczyk2000@gmail.com>
2025-02-22 16:56:20 +0100

    Add component presenting evenry project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationSidebarComponent } from './component/navigation/navigation-sidebar/navigation-sidebar.component';
 import { ProjectDescriptionLayoutComponent } from './component/project-description-layout/project-description-layout.component';
-import { ProjectLayoutComponent } from './component/project/project-layout/project-layout.component';
+import { ProjectsComponent } from './component/project/projects/projects.component';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +10,7 @@ import { ProjectLayoutComponent } from './component/project/project-layout/proje
     RouterOutlet, 
     ProjectDescriptionLayoutComponent,
     NavigationSidebarComponent,
-    ProjectLayoutComponent
+    ProjectsComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
---
+++ src/app/component/project/projects/projects.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ProjectLayoutComponent } from '../project-layout/project-layout.component';
+
+@Component({
+  selector: 'app-projects',
+  imports: [
+    ProjectLayoutComponent
+  ],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.scss'
+})
+export class ProjectsComponent {
+
+}
