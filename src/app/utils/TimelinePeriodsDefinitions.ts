4085bc708d95c4701ce2694ea4fddebc4958f500
bartlomiej.kucharczy <bartlomiej.kucharczyk@envelo.pl>
2025-02-24 16:03:19 +0100

    add timeline education period component

--- src/app/utils/TimelinePeriodsDefinitions.ts
+++ src/app/utils/TimelinePeriodsDefinitions.ts
@@ -1,7 +1,11 @@
-export var periods: TimelinePeriod[] = [
+export var workPeriods: TimelinePeriod[] = [
     { from: new Date(2021, 6, 1), to: new Date(2021, 9, 30) },
     { from: new Date(2021, 10, 1), to: new Date(2023, 9, 30) },
-    { from: new Date(2023, 10, 1)}
+    { from: new Date(2023, 10, 1) }
+]
+
+export var educationPeriods: TimelinePeriod[] = [
+    { from: new Date(2019, 9, 1), to: new Date(2023, 5, 20) }
 ]
 
 export interface TimelinePeriod {
