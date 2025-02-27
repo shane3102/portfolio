1c04bc2be5cd484d8ecd53a6aab4c103421179a1
shane3102 <bkucharczyk2000@gmail.com>
2025-02-27 12:36:50 +0100

    Add work/education logos and attach those to timeline + add time period

--- src/app/component/timeline/timeline-period-info/timeline-period-info.component.ts
+++ src/app/component/timeline/timeline-period-info/timeline-period-info.component.ts
@@ -1,12 +1,18 @@
 import { Component, Input } from '@angular/core';
+import { TimelinePeriod } from '../../../utils/TimelinePeriodsDefinitions';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-timeline-period-info',
-  imports: [],
+  imports: [
+    DatePipe
+  ],
   templateUrl: './timeline-period-info.component.html',
   styleUrl: './timeline-period-info.component.scss'
 })
 export class TimelinePeriodInfoComponent {
 
   @Input() width: string = ""
+  @Input() timelinePeriod!: TimelinePeriod
+
 }
