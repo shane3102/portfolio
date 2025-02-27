976866671ed383ccd2803ccf8f06859e7573d50f
shane3102 <bkucharczyk2000@gmail.com>
2025-02-27 13:47:18 +0100

    Add titles to timelines info, change it's sizes

--- src/app/utils/TimelinePeriodsDefinitions.ts
+++ src/app/utils/TimelinePeriodsDefinitions.ts
@@ -1,15 +1,35 @@
 export var workPeriods: TimelinePeriod[] = [
-    { from: new Date(2021, 6, 1), to: new Date(2021, 9, 30), logo: "/logos/work-and-education/envelo.svg" },
-    { from: new Date(2021, 10, 1), to: new Date(2023, 9, 30), logo: "/logos/work-and-education/envelo.svg" },
-    { from: new Date(2023, 10, 1), logo: "/logos/work-and-education/envelo.svg" }
+    {
+        from: new Date(2021, 6, 1),
+        to: new Date(2021, 9, 30),
+        logo: "/logos/work-and-education/envelo.svg",
+        title: "Akademia envelo (staż)",
+    },
+    {
+        from: new Date(2021, 10, 1),
+        to: new Date(2023, 9, 30),
+        logo: "/logos/work-and-education/envelo.svg",
+        title: "Młodszy specjalista ds. rozwoju oprogramowania",
+    },
+    {
+        from: new Date(2023, 10, 1),
+        logo: "/logos/work-and-education/envelo.svg",
+        title: "Specjalista ds. rozwoju oprogramowania",
+    }
 ]
 
 export var educationPeriods: TimelinePeriod[] = [
-    { from: new Date(2019, 9, 1), to: new Date(2023, 5, 20), logo: "/logos/work-and-education/SGGW.svg" }
+    {
+        from: new Date(2019, 9, 1),
+        to: new Date(2023, 5, 20),
+        logo: "/logos/work-and-education/SGGW.svg",
+        title: "Studia inż. (ocena końcowa: 5.0)",
+    }
 ]
 
 export interface TimelinePeriod {
     from: Date,
     to?: Date,
-    logo: string
+    logo: string,
+    title?: string
 }
