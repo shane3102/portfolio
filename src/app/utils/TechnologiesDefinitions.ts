bee62ceb83b4f29bdcc99f9b618ba156dce3ad99
shane3102 <bkucharczyk2000@gmail.com>
2025-02-27 12:36:50 +0100

    Add work/education logos and attach those to timeline + add time period

--- src/app/utils/TechnologiesDefinitions.ts
+++ src/app/utils/TechnologiesDefinitions.ts
@@ -4,20 +4,20 @@ export class TechnologiesDefinitions {
     static technologiesDefinitions: Map<technology, TechnologyInfo> = new Map(
         [
             // Language
-            ["Java", { logo: "/logos/java.svg", link: "https://www.java.com/pl/" }],
-            ["Kotlin", { logo: "/logos/kotlin.svg", link: "https://kotlinlang.org/" }],
-            ["Typescript", { logo: "/logos/typescript.svg", link: "https://www.typescriptlang.org/" }],
+            ["Java", { logo: "/logos/technologies/java.svg", link: "https://www.java.com/pl/" }],
+            ["Kotlin", { logo: "/logos/technologies/kotlin.svg", link: "https://kotlinlang.org/" }],
+            ["Typescript", { logo: "/logos/technologies/typescript.svg", link: "https://www.typescriptlang.org/" }],
 
             // Framework
-            ["Angular", { logo: "/logos/angular.svg", link: "https://angular.dev/" }],
-            ["Spring Boot", { logo: "/logos/spring.svg", link: "https://spring.io/projects/spring-boot" }],
-            ["Quarkus", { logo: "/logos/quarkus.svg", link: "https://quarkus.io/" }],
+            ["Angular", { logo: "/logos/technologies/angular.svg", link: "https://angular.dev/" }],
+            ["Spring Boot", { logo: "/logos/technologies/spring.svg", link: "https://spring.io/projects/spring-boot" }],
+            ["Quarkus", { logo: "/logos/technologies/quarkus.svg", link: "https://quarkus.io/" }],
 
             // Other
-            ["PostgreSQL", { logo: "/logos/postgresql.svg", link: "https://www.postgresql.org/" }],
-            ["MySql", { logo: "/logos/mysql.svg", link: "https://www.mysql.com/" }],
-            ["Redis", { logo: "/logos/redis.svg", link: "https://redis.io/" }],
-            ["RabbitMQ", { logo: "/logos/rabbitmq.svg", link: "https://www.rabbitmq.com/" }],
+            ["PostgreSQL", { logo: "/logos/technologies/postgresql.svg", link: "https://www.postgresql.org/" }],
+            ["MySql", { logo: "/logos/technologies/mysql.svg", link: "https://www.mysql.com/" }],
+            ["Redis", { logo: "/logos/technologies/redis.svg", link: "https://redis.io/" }],
+            ["RabbitMQ", { logo: "/logos/technologies/rabbitmq.svg", link: "https://www.rabbitmq.com/" }],
         ]
     )
 
--- src/app/utils/TimelinePeriodsDefinitions.ts
+++ src/app/utils/TimelinePeriodsDefinitions.ts
@@ -1,14 +1,15 @@
 export var workPeriods: TimelinePeriod[] = [
-    { from: new Date(2021, 6, 1), to: new Date(2021, 9, 30) },
-    { from: new Date(2021, 10, 1), to: new Date(2023, 9, 30) },
-    { from: new Date(2023, 10, 1) }
+    { from: new Date(2021, 6, 1), to: new Date(2021, 9, 30), logo: "/logos/work-and-education/envelo.svg" },
+    { from: new Date(2021, 10, 1), to: new Date(2023, 9, 30), logo: "/logos/work-and-education/envelo.svg" },
+    { from: new Date(2023, 10, 1), logo: "/logos/work-and-education/envelo.svg" }
 ]
 
 export var educationPeriods: TimelinePeriod[] = [
-    { from: new Date(2019, 9, 1), to: new Date(2023, 5, 20) }
+    { from: new Date(2019, 9, 1), to: new Date(2023, 5, 20), logo: "/logos/work-and-education/SGGW.svg" }
 ]
 
 export interface TimelinePeriod {
     from: Date,
     to?: Date,
+    logo: string
 }
