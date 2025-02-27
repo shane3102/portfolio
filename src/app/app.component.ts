368d3e0cc2c4fea40752764a0d2c49c17c6e6ad5
shane3102 <bkucharczyk2000@gmail.com>
2025-02-27 15:52:37 +0100

    add component with main info, add some imgs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProjectsComponent } from './component/project/projects/projects.component';
 import { TimelineSectionComponent } from './component/timeline/timeline-section/timeline-section.component';
+import { MainInfoComponent } from './component/main-info/main-info.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet, 
     ProjectsComponent,
-    TimelineSectionComponent
+    TimelineSectionComponent,
+    MainInfoComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
